9d811038ff710053fae47ce0f3a6bb29f454368b
Andrew Russo and Inthrayuth Mahaphol <andrew@>
2016-04-21 16:36:37 -0700

    Streamlined the code for Anagram.java to run.

--- src/main/java/Anagram.java
+++ src/main/java/Anagram.java
@@ -3,19 +3,13 @@ import java.util.Arrays;
 public class Anagram {
 
   public Boolean checkAnagram(String word1, String word2) {
-    // word1 = word1.toLowerCase();
-    // word2 = word2.toLowerCase();
 
     char[] word1Array = word1.toLowerCase().toCharArray();
     char[] word2Array = word2.toLowerCase().toCharArray();
-
+    
     Arrays.sort(word1Array);
     Arrays.sort(word2Array);
 
-    if (Arrays.equals(word1Array, word2Array)) {
-      return true;
-    } else {
-      return false;
-    }
+    return Arrays.equals(word1Array, word2Array);
   }
 }
